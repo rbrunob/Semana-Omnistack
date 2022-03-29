08ec49d107e2bf153d84332e7a11c968be51e007
Bruno Ramos <79422907+rbrunob@users.noreply.github.com>
2022-03-29 20:19:44 -0300

    asd

--- src/route.js
+++ src/route.js
@@ -3,6 +3,6 @@ const SessionControler = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/users', SessionControler.store)
+routes.post('/sessions', SessionControler.store)
  
  module.exports = routes;
