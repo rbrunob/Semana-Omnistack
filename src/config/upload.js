ff7f2928f4c1ef4504e05c22162f1a3f048c3679
Bruno Ramos <79422907+rbrunob@users.noreply.github.com>
2022-03-29 22:06:33 -0300

    123

--- src/config/upload.js
+++ src/config/upload.js
@@ -3,14 +3,14 @@ const { dirname } = require('path');
 const path = require('path');
 
 module.exports = {
-Storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        const name = path.basename(file.originalname, ext);
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        filename: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext);
 
-        cb(null, `${name}-${Date.now()}${ext}`)
-    },
-})
+            cb(null, `${name}-${Date.now()}${ext}`);
+        },
+    }),
 
-}
+};
