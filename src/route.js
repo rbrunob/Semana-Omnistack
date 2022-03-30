8f80dd275e258ddaf59514fbd4ca5d593a3ef958
Bruno Ramos <79422907+rbrunob@users.noreply.github.com>
2022-03-29 21:44:59 -0300

    update

--- src/route.js
+++ src/route.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+const upload = require('./config/upload');
+const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
+const uploads = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store)
 
-routes.post('/spots', SpotController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
  module.exports = routes;
