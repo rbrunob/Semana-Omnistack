cfcabe6f218234233a41ca32a79d957d03319a3c
Bruno Ramos <79422907+rbrunob@users.noreply.github.com>
2022-03-29 21:50:26 -0300

    foi

--- src/route.js
+++ src/route.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const multer = require('multer');
-const upload = require('./config/upload');
 const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
-const uploads = multer(uploadConfig);
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store)
 
-routes.post('/spots', uploads.single('thumbnail'), SpotController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
  module.exports = routes;
